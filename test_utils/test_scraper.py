284986e138c871c62c2a3cafa2427a00be4e0298
Juan José Serrano Gutiérrez <juanjo.jjserra@gmail.com>
2023-01-02 12:39:43 +0100

    Fix flake8 errors in test_scraper.py

--- test_utils/test_scraper.py
+++ test_utils/test_scraper.py
@@ -1,25 +1,30 @@
 import pytest
 from utils.scraper import Scraper
 
+
 @pytest.fixture
 def scraper():
 
     return Scraper()
+
 
 @pytest.fixture
 def url():
 
     return 'https://www.skysports.com/premier-league-table'
 
+
 @pytest.fixture
 def identifier():
 
     return {'class': 'standing-table__table'}
 
+
 @pytest.fixture
 def headers():
 
     return ['#', 'Team', 'Pl', 'W', 'D', 'L', 'F', 'A', 'GD', 'Pts', 'Last 6']
+
 
 def test_get_table_by_identifier_when_headers_are_unknown(
     scraper, url, identifier, headers
@@ -28,6 +33,7 @@ def test_get_table_by_identifier_when_headers_are_unknown(
     table = scraper.get_table_by_identifier(url, identifier)
     assert len(table) == 20
     assert all(table.columns == headers)
+
 
 def test_get_table_by_identifier_when_headers_are_known(
     scraper, url, identifier, headers
